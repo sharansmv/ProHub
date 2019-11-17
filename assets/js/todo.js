5c0af1d8ddf21e7ea2944fa0ba8fa6c898b930a1
sharansmv <sharansmv10@gmail.com>
2019-11-17 14:59:52 +0530

    Create and delete tasks functionality added.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,5 +1,20 @@
 //adding function to click on task/li and check them off
-$("li").click(function(){
-    $(this).css("color", "green");
-    $(this).css("text-decoration", "line-through");
+$("ul").on("click","li",function(){
+   $(this).toggleClass("completed");
 });
+
+//deleting a task by clicking on X
+$("ul").on("click","span",function(event){
+    $(this).parent().remove();
+    event.stopPropagation();
+});
+
+//adding a new task on hitting enter
+$("input").keypress(function(event){
+    if(event.which === 13){
+        var task = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span>" + task + "</li>");
+    }
+});
+
